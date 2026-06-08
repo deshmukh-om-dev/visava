b0affe1f98d09bf8342ac1bbeff9813074df656f
Om Deshmukh <deshmukh.omworks@gmail.com>
2026-06-08 21:52:31 +0530

    Initialized Spring Boot backend

---
+++ backend/src/main/java/com/visava/visava_backend/VisavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.visava.visava_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisavaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/visava/visava_backend/VisavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visava.visava_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
